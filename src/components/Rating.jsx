16f5566cd126ae7e8cd796bde2b90fe09c582d13
Sayuru Dev <mailtosayuru@gmail.com>
2025-08-28 01:40:45 -0400

    rating finished

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,10 @@
+import { useState} from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
   return (
@@ -6,7 +12,11 @@ const Rating = () => {
       <h2>Rate Your Experience</h2>
       <div className="stars">
         {stars.map((star) => (
-          <span key={star} className="star">
+          <span onClick={()=> setRating(star)} 
+          onMouseEnter ={() => setHover(star)}
+          onMouseLeave ={() => setHover(0)}
+
+          key={star} className="star">
             {'\u2605'}
           </span>
         ))}
